92c1006699ed974ebe927bf62e11f587e748885c
PodYapolskiy <podyapolskiyaa@gmail.com>
2025-06-05 18:48:01 +0300

    fix: fix on fix, the problem was another

--- scripts/server.extraction.bash
+++ scripts/server.extraction.bash
@@ -8,8 +8,8 @@ uv run src/extraction.py \
     --preprocessed-val-dir preprocessed_val_data \
     --train-csv train_data.csv \
     --val-csv val_data.csv \
-    --extract-video 0 \
-    --extract-audio 1 \
-    --extract-text 1 \
     --video-model-device cpu \
     --text-model-device cpu \
+    --extract-text \
+    --extract-audio \
+    # --extract-video \
