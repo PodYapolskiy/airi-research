935ee73ced3f7fa5b24df84dc5dba0f8372f9259
PodYapolskiy <podyapolskiyaa@gmail.com>
2025-06-11 13:40:01 +0300

    feat: run experiments for only text modality on personality labels

--- scripts/training/personality/text.bash
+++ scripts/training/personality/text.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-uv run src/train_audio_personality.py \
+uv run src/train_text_personality.py \
     --data-dir /home/HDD12TB/datasets/images/emotions/ACMMM25/AVI/AVI_Challenge_dataset \
-    --batch-size 128 \
+    --batch-size 450 \
     --num-workers 4 \
     --device cuda \
-    --epochs 25 \
+    --epochs 100 \
     --lr 0.001 \
     --trait "Conscientiousness" \
-    --only-dim 512
+    --only-dim 1024
